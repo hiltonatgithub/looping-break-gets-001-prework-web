1a80cb5af4a812dbfb847582da19fadaabfdbf0d
hilton mak <hiltonmak@yahoo.om>
2016-06-27 03:24:18 +0000

    Done.

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,10 @@
 
-def levitation_quiz
-	#your code here
-end
+#def levitation_quiz
 
+	def levitation_quiz
+	    puts "What is the spell that enacts levitation?"
+	    name = gets
+	    puts "You passed the quiz!\n"
+	end
 
+#end
